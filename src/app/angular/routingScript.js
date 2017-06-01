1e62ae70f7dfc26dc37b72d8e4d73f4d2afc4a8f
pasindunirmal <nirmal.ariyathilake@my.sliit.lk>
2017-06-01 19:00:40 +0530

    PCU Client

---
+++ src/app/angular/routingScript.js
@@ -0,0 +1,44 @@
+/**
+ * Created by Nirmal on 4/29/2017.
+ */
+var app = angular.module("Route", ["ngRoute"])
+    .config(function ($routeProvider, $locationProvider) {
+        $routeProvider
+            .when("/", {
+                templateUrl: "pages/home.html"
+            })
+            .when("/index.html", {
+                templateUrl: "pages/home.html"
+            })
+            .when("/pcusheet/admission", {
+                templateUrl: "pages/pcusheet/admission.html",
+                controller: "admissionController"
+            })
+            .when("/pcusheet/addprescription", {
+                templateUrl: "pages/pcusheet/addprescription.html",
+                controller:"pcuController"
+            })
+            .when("/registration", {
+                templateUrl: "pages/registration.html"
+            })
+            .when("/pcusheet", {
+                templateUrl: "pages/pcusheet/pcusheet.html",
+                controller : "pcuController"
+            })
+            .when("/search", {
+                templateUrl: "pages/searchpatient.html"
+            })
+            .when("/pcusheet/addlabtest", {
+                templateUrl: "pages/pcusheet/addlabtest.html"
+            })
+
+
+            // .when("/pagenotfound", {
+            //     templateUrl: "pages/error/404.html"
+            // })
+            .otherwise({redirectTo: '/'})
+        $locationProvider.html5Mode({
+            enabled: true,
+            requireBase: false
+        });
+    })
