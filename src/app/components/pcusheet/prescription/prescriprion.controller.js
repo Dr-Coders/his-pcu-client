07d4a1b4293d972fb4b1ef32806419e714b75525
NirmalAriyathilake <nirmal.ariyathilake@my.sliit.lk>
2017-07-01 00:13:57 +0530

    PCU Client

--- src/app/components/pcusheet/prescription/prescriprion.controller.js
+++ src/app/components/pcusheet/prescription/prescriprion.controller.js
@@ -2,7 +2,7 @@
  * Created by Nirmal on 6/29/2017.
  */
 
-app.controller('PrescriptionController',['$scope','PrescriptionService','$filter',function ($scope,PrescriptionService,$filter) {
+app.controller('PrescriptionController',['$scope','PrescriptionService','DoctorService','$filter',function ($scope,PrescriptionService,DoctorService,$filter) {
     $scope.loadPrescription = function () {
         PrescriptionService.getPrescriptions()
             .then(function (response) {
@@ -34,4 +34,29 @@ app.controller('PrescriptionController',['$scope','PrescriptionService','$filter
     $scope.formatDate = function(date){
         return $filter('date')(new Date(date) , "dd MMM, yyyy ");
     };
+
+    $scope.showDate = function (date) {
+        if(date != $scope.previourDate){
+            $scope.previourDate = date;
+            return true;
+        }
+        else
+            return false;
+    };
+
+
+    $scope.loadDoctors = function() {
+        DoctorService.getDoctors()
+            .then(function (response) {
+                console.log("I got the data I requested : " + response.data);
+                $scope.doctordata = response.data;
+            });
+    };
+    $scope.loadDoctors();
+
+    $scope.showDoctor = function (doctor_id) {
+        $scope.selecteddoctordata = $filter('filter')($scope.doctordata, {_id: doctor_id })[0];
+        console.log("Selected doctor : " + doctor_id + " data : " + $scope.selecteddoctordata.firstname);
+    }
+
 }]);
